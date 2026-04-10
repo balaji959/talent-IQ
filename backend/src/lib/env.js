c83fa7485f04c86c9b4115e9326341d99e69b9c8
balaji prakash mahanta <balajimahanta148@gmail.com>
2026-04-10 14:08:40 +0530

    deployment config added

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -4,6 +4,7 @@ dotenv.config(); // This loads the variables from your .env file
 
 export const ENV = {
   PORT: process.env.PORT || 3000, // Fallback to 3000 if PORT is missing
-  DB_URL: process.env.DB_URL
+  DB_URL: process.env.DB_URL,
+  NODE_ENV: process.env.NODE_ENV || 'development', // Default to 'development' if NODE_ENV is missing
 };
 
