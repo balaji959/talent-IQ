de5c5586f6bae66d0d1eab1e55fb5c8bfc6f2c68
balaji prakash mahanta <balajimahanta148@gmail.com>
2026-04-10 13:47:21 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,28 @@
 
 import express from 'express';
+import path from 'path';
 import { ENV } from './lib/env.js';
-// ONLY ONE OF THESE!
 const app = express(); 
-
+const __dirname = path.resolve();
 // ... rest of your code ...
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'success from api' });
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: 'success from' });
 });
+app.get("/books", (req, res) => {
+  res.status(200).json({ message: 'this is the book endpoint' });
+});
+// make our app ready for deployment
+if (ENV.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.listen(ENV.PORT, () => {
-  console.log('SERVER STARTED ON PORT' + ENV.PORT);
+app.get('/{*any}', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+});
+}
+
+const PORT = ENV.PORT || 5001; // Fallback to 5001 if ENV.PORT is missing
+
+app.listen(PORT, () => {
+  console.log(`✅ SERVER RUNNING AT: http://localhost:${PORT}`);
+  console.log(`Try health check at: http://localhost:${PORT}/health`);
 });
